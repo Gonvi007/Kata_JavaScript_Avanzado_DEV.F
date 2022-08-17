7a23df2d05077e74f7c84cf72386a4fe3e77396a
Alexis Gonzalez <alesisgonzalez50@gmail.com>
2022-08-16 20:11:44 -0500

    Primera parte lista

--- Promesas/xmlhttprequest.js
+++ Promesas/xmlhttprequest.js
@@ -1,1 +1,29 @@
+"use strict";
 
+//require === importar
+const XMLHTTPRequest = require("xmlhttprequest").XMLHttpRequest;
+
+const url1 = "https://pokeapi.co/api/v2/pokemon/pikachu";
+
+function getData(url, callback) {
+  let xhttp = new XMLHTTPRequest();
+  
+  xhttp.open('GET', url, true);
+
+  xhttp.onreadystatechange = () => {
+    if (xhttp.readyState === 4) {
+      if (xhttp.status === 200) {
+        callback(JSON.parse(xhttp.responseText), null)
+        return ;
+      } else {
+        callback(null, "Algo salio mal intente mas tarde")
+      }
+    }
+  }
+
+  xhttp.send();
+}
+
+getData(url1, (data, error) => {
+  console.log(data, error);
+});
