084c7df9fdd13257871f5d62a8c144c3ae3f114f
Alexis Gonzalez <alesisgonzalez50@gmail.com>
2022-08-24 15:08:10 -0500

    Retomando la clase de martes 08 2022

--- Crud/index.js
+++ Crud/index.js
@@ -2,15 +2,18 @@
 
 const d = document
 
-const input_todo = d.getElementById('input-todo') 
-const btn_add = d.getElementById('btn-add') 
+// Capturando los nodos de html con JS
+const input_todo = d.getElementById('input-todo')
+const btn_add = d.getElementById('btn-add')
 const content_w = d.getElementById('content-wrapper')
 
+// URL base para consumir API
 const url = 'http://localhost:3000/data'
 
-const printTask = () => {
+// Funcion que imprime li y button en el DOM
+const printTask = (task) => {
   let li = d.createElement('li')
-  li.textContent = "Hola, soy una tarea"
+  li.textContent = task
 
   const btn_1 = d.createElement('button')
   const btn_2 = d.createElement('button')
@@ -30,4 +33,37 @@ const printTask = () => {
   content_w.appendChild(li)
 }
 
-printTask()
+// Funcion que obtine datos de la API
+const getData = () => {
+  return fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      data.array.forEach(element => {
+        printTask(element.task)        
+      });
+      // return console.log(data); 
+    })
+    .catch(error => console.error(error))
+}
+
+// // Funcion que envia datos a la API
+// const postData = (task) => {
+//   return fetch(url, {
+//     method: 'POST',
+//     header: {
+//       'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify({
+//       'task': task
+//     })
+//   })
+//   .then(response => response.json())
+//   .then(data => console.log(data))
+//   .chatch(error => console.error(error))
+// }
+// postData();
+// getData();
+
+// btn_add.addEventListener('click', () => {
+//   postData(input_todo.value);
+// })
--- async-await/index.js
+++ async-await/index.js
@@ -1,9 +1,34 @@
+// Sirve para poder hacer que una funcion sincrona, pueda acceder a los valores de na funcion asincrona
 const url_base = 'https://jsonplaceholder.typicode.com/users'
 
-const getData = async (url) => {
-  const response = await fetch(url)
-  const data = await response.json()
-  return data
-}
 
-getData(url_base)
+const getData = async (url) => {
+  try {
+    const response = await fetch(url)
+    const data =  await response.json()
+    return data
+  } catch (error) {
+    console.error(error)
+    return error
+  }
+ 
+}
+const showData = async () => {
+  const finalData = await getData(url_base)
+  return console.log(finalData)
+}
+showData()
+/*
+const getData = async (url) => {
+  return fetch(url_base)
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error))
+}
+*/
+
+// const showData = () => {
+//   const finalData = getData()
+//   return finalData
+// }
+// console.log(showData())
